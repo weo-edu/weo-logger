2f85ea0681215000b5681ef1990d8d25ac12bb04
Andrew Shaffer <andrew@weo.io>
2015-01-23 16:33:40 -0800

    only use papertrail if the env vars are set

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ var winston = require('winston');
 require('winston-papertrail').Papertrail;
 
 module.exports = function(appName) {
+  var transports = [];
+  if(process.env.PAPERTRAIL_HOST) {
+    transports.push(new winston.transports.Papertrail({
+      host: process.env.PAPERTRAIL_HOST,
+      port: Number(process.env.PAPERTRAIL_PORT),
+      handleExceptions: true,
+      colorize: true,
+      program: appName
+    }));
+  }
+
   var logger = new winston.Logger({
-    transports: [
-      new winston.transports.Papertrail({
-          host: process.env.PAPERTRAIL_HOST,
-          port: process.env.PAPERTRAIL_PORT,
-          handleExceptions: true,
-          colorize: true,
-          program: appName
-      })
-    ]
+    transports: transports
   });
 
   var write = process.stdout.write;
